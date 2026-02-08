40e6c328e5bab5af48707ce337e5391c6ebec820
artes26 <ayush16r@gmail.com>
2026-02-08 21:05:19 +0530

    day2 login2

---
+++ phase-1/1- Control flow/forLoop.java
@@ -0,0 +1,19 @@
+public class forLoop {
+    public static void main(String[] args) {
+        // q - WAP to print first n natural number ''
+
+        // int n=5;
+        // for(int i=1; i<+n; i++) {
+        //     System.out.println(2*i+1);
+       // }
+
+       int n=5;
+        for(int i=1; i<5;i++) {
+            System.out.println(i);
+            System.out.println("java is great");
+            if (i==3) {
+                System.out.println("ending the loop");
+              break;  
+            }
+    }
+}}
--- phase-1/1- Control flow/whileLoop.java
+++ phase-1/1- Control flow/whileLoop.java
@@ -1,3 +1,10 @@
 public class whileLoop {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a<=2) {
+         System.out.println(a);
+         a++;
+        }
+    }
     
 }
