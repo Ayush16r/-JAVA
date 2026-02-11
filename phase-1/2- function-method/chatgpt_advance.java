e9691c23913055af87a645ed16e70eb578dd895b
artes26 <ayush16r@gmail.com>
2026-02-11 20:01:00 +0530

    max number concept

---
+++ phase-1/2- function-method/chatgpt_advance.java
@@ -0,0 +1,20 @@
+public class chatgpt_advance {
+static int max(int x, int y, int z){
+    int largest=x;
+    if (y>x) {
+        largest=y;
+        
+    }
+    if (z>largest) {
+        largest =z;
+        
+    }
+    return largest;
+
+
+}
+public static void main(String[] args) {
+    int result=max(3, 5, 1);
+    System.out.println(result);
+}
+}
