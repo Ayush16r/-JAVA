28e3e906b3bd818536238d0249d22380f88ec3be
artes26 <ayush16r@gmail.com>
2026-02-12 09:23:25 +0530

    t

---
+++ phase-1/advance_q_set.java/q1.java
@@ -0,0 +1,11 @@
+public class q1 {
+    // second largest without using sort()
+    public static void main(String[] args) {
+        int[] arr={10,20,20,30,5,9};
+        int largest = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        
+
+} 
+    }
+
